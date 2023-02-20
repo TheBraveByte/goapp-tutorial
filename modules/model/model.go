60788ffc744f309b71ddb05ca82d5f4d2d87547d
dev-ayaa <ayaaakinleye@gmail.com>
2023-02-20 11:37:05 +0100

    modify user model

--- modules/model/model.go
+++ modules/model/model.go
@@ -1,15 +1,16 @@
 package model
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
 	ID         primitive.ObjectID `json:"_id" bson:"_id"`
 	Name       string             `json:"name" Usage:"required"`
 	Email      string             `json:"email" Usage:"required,alphanumeric"`
-	Age        string             `json:"sex"`
+	Age        int                `json:"age"`
 	Password   string             `json:"password" Usage:"required,alphanumeric"`
 	Occupation string             `json:"occupation"`
 	CreatedAt  time.Time          `json:"created_at"`
